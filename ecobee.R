706330a494893afb33335347ea9f66951b47c445
rozran00@gmail.com <rozran00@gmail.com>
2021-02-21 20:01:46 -0500

    updating timing for heat in bedroom

--- ecobee.R
+++ ecobee.R
@@ -66,7 +66,7 @@ is_it_weekend <- ifelse(weekdays(Sys.Date()) %in% c("Saturday", "Sunday"), TRUE,
 current_time <- hour(Sys.time()) + (minute(Sys.time()) / 60)
 is_afternoon_nap <- ifelse(current_time >= 16 & current_time <= 18, TRUE, FALSE)
 is_sleeptime <- ifelse(current_time >= 0.5 & current_time <= 11, TRUE, FALSE)
-is_sleeptime_parents_early <- ifelse(current_time >= 23.5 | current_time <= 3, TRUE, FALSE)
+is_sleeptime_parents_early <- ifelse(current_time >= 0.5 & current_time <= 3, TRUE, FALSE)
 is_sleeptime_parents_late <- ifelse(current_time > 3 & current_time <= 11, TRUE, FALSE)
 is_worktime <- ifelse(current_time >= 13, TRUE, FALSE)
 
